3a2532d9c6cb771b077bce31dbceb1ca64e48948
fistoka-js <j.p.sverdlin@gmail.com>
2026-06-30 00:26:08 -0400

    Refactor get_connection to use environment variables

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -4,7 +4,7 @@ import os
 def get_connection():
     return psycopg2.connect(
         host=os.environ.get("DB_HOST", "localhost"),
-        user=os.environ.get("DB_USER", "jacob"),
-        password=os.environ.get("DB_PASSWORD", "password123"),
-        dbname=os.environ.get("DB_NAME", "testgrader")
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        dbname=os.environ.get("DB_NAME")
     )
